eba8d63ae7151b82cfb8197f69126bb6b9f890a0
hyunsoo han <dreamh1123@naver.com>
2018-07-28 21:10:11 +0900

    setting commit

---
+++ mangolist/HomeController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  mangolist
+//
+//  Created by hyunsu han on 2018. 7. 28..
+//  Copyright © 2018년 hyunsoo. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
